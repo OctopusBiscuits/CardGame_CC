46c6f2d6490b22865a9dc53a847be4a4c441834b
OctopusBiscuits <134218297+OctopusBiscuits@users.noreply.github.com>
2024-12-13 11:42:51 +0000

    health beta
    skeleton

---
+++ Assets/Scripts/SliderManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SliderManager : MonoBehaviour
+{
+    public GameObject JoshCubeGO;
+    JoshCube Jcube;
+    private void Start()
+    {
+        //JoshCubeGO = GameObject.FindWithTag("Enemy");
+        //Jcube = JoshCubeGO.GetComponent<JoshCube>();
+    }   Jcube.
+ 
+    
+    public class EnemyHealthSlider
+    {
+        public Slider ESHP;
+        //public static Vector3 Jcube.health;
+        //int EHealth = Jcube.health;
+        
+          public void SliderChange()
+        {
+            //ESHP.value = SlideHP;
+        }
+    }
+}
