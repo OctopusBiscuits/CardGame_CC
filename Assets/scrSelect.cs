cc24cb32ba515a7f9ffe9ab9045864ef997a8c37
JChambers13 <56290403+JChambers13@users.noreply.github.com>
2024-12-18 21:58:34 +0000

    Update scrSelect.cs

--- Assets/scrSelect.cs
+++ Assets/scrSelect.cs
@@ -7,6 +7,7 @@ public class scrSelect : MonoBehaviour
     public bool selectMode = false;
     GameObject delete;
     public string selectedEnemy;
+
     // Start is called before the first frame update
     void Start()
     {
